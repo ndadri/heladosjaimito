f07a058694ebad2203b61fdc7bbf192c37f673ed
ndadri <adrianfalcones08@gmail.com>
2024-10-22 19:22:51 -0500

    jaimitov3

--- js/jaimito.js
+++ js/jaimito.js
@@ -1,2 +1,2 @@
 var nombre = prompt("Antes de continuar tienes que decirnos como quieres que te llamemos."); //alerta de inicio de pagina
-document.write("Bienvenido ", nombre, " a la pagina web oficial de la heladeria Jaimito🐧🍦🍧");//nombre de bienvenida
+document.write("Bienvenido ", nombre, " a la pagina web oficial de la heladeria Jaimito🐧🍦🍧", "<br>");//nombre de bienvenida
