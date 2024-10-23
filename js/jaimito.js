64a5349965e1818980b01c166b29d756b2f11083
ndadri <adrianfalcones08@gmail.com>
2024-10-23 01:46:41 -0500

    jaimitov5

--- js/jaimito.js
+++ js/jaimito.js
@@ -1,2 +1,24 @@
 var nombre = prompt("Antes de continuar tienes que decirnos como quieres que te llamemos."); //alerta de inicio de pagina
 document.write("Bienvenido ", nombre, " a la pagina web oficial de la heladeria Jaimito🐧🍦🍧", "<br>");//nombre de bienvenida
+
+function mostrarAlertaFace(event) {
+    event.preventDefault(); // Evita que el enlace redirija inmediatamente
+    const confirmacion = confirm("¿Estás seguro de que deseas ir a Facebook?");
+    if (confirmacion) {
+        window.location.href = event.target.href; // Redirige al enlace si el usuario acepta
+    }
+}
+function mostrarAlertaWhat(event) {
+    event.preventDefault(); // Evita que el enlace redirija inmediatamente
+    const confirmacion = confirm("¿Estás seguro de que deseas ir a WhatsApp?");
+    if (confirmacion) {
+        window.location.href = event.target.href; // Redirige al enlace si el usuario acepta
+    }
+}
+function mostrarAlertaUbi(event) {
+    event.preventDefault(); // Evita que el enlace redirija inmediatamente
+    const confirmacion = confirm("¿Estás seguro de que deseas ver la Ubicación?");
+    if (confirmacion) {
+        window.location.href = event.target.href; // Redirige al enlace si el usuario acepta
+    }
+}
